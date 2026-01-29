9db18b7b6162128ed85348b338f54bd686ea4ca0
jefrisaputra <jefrisaputra7373@gmail.com>
2026-01-29 14:28:30 +0700

    Update GOKasir

--- models/product.go
+++ models/product.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Product struct {
-	ID         uint      `json:"id" gorm:"primaryKey"`
-	Name       string    `json:"name" gorm:"size:100;not null"`
-	Price      float64   `json:"price" gorm:"not null"`
-	Stock      int       `json:"stock" gorm:"not null"`
-	CategoryID uint      `json:"category_id" gorm:"not null;index"`
-	Category   Category  `json:"category" gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID    uint    `json:"id" gorm:"primaryKey"`
+	Name  string  `json:"name" gorm:"size:100;not null"`
+	Price float64 `json:"price" gorm:"not null"`
+	Stock int     `json:"stock" gorm:"not null"`
+	//CategoryID uint      `json:"category_id" gorm:"not null;index"`
+	Category  Category  `json:"category" gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
