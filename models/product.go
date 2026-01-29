37434da171354ff1f50d29baeb90ef8bc315c83b
jefrisaputra <jefrisaputra7373@gmail.com>
2026-01-29 14:40:09 +0700

    Update GOKasir

--- models/product.go
+++ models/product.go
@@ -7,7 +7,7 @@ type Product struct {
 	Name       string    `json:"name" gorm:"size:100;not null"`
 	Price      float64   `json:"price" gorm:"not null"`
 	Stock      int       `json:"stock" gorm:"not null"`
-	CategoryID uint      `json:"category_id" gorm:"not null;index"`
+	CategoryID uint      `json:"-" gorm:"not null;index"` // <-- tambah json:"-"
 	Category   Category  `json:"category" gorm:"constraint:OnUpdate:NO ACTION,OnDelete:SET NULL;"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
